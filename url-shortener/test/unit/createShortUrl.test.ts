2a2885d9f77bae8532a45f5559568e35a810e1fe
Deepali Verma <deepaliv679@gmail.com>
2023-11-26 23:15:31 +0530

    added hello world testcase

--- url-shortener/test/unit/createShortUrl.test.ts
+++ url-shortener/test/unit/createShortUrl.test.ts
@@ -1,11 +1,48 @@
 import { AppService } from "../../src/service/app.service";
+import { getModelToken } from "@nestjs/mongoose";
+import { Test, TestingModule } from "@nestjs/testing";
+import { Model } from "mongoose";
 
 describe("AppService", () => {
-  let appController: AppService;
+  let appService: AppService;
+  let modelMock: Model<unknown>;
 
-  describe("root", () => {
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AppService,
+        {
+          provide: getModelToken("ShortUrl"),
+          useValue: {
+            findOne: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    appService = module.get<AppService>(AppService);
+    modelMock = module.get<Model<unknown>>(getModelToken("ShortUrl"));
+  });
+
+  describe("getHello", () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe("Hello World!");
+      expect(appService.getHello()).toBe("Hello World!");
     });
   });
+
+//   describe("getFullUrl", () => {
+//     it('should throw NotFoundException for non-existing shortUrl', async () => {
+//       // Mock the findOne method to return null, simulating a non-existing shortUrl
+//       modelMock.findOne.mockReturnValue({_id:6, full: "ghjh", short: "hjj"});
+
+//       // Assuming you have a valid 'data' object for testing
+//       const data = { shortUrl: "nonExistingShortUrl" };
+
+//       await expect(appService.getFullUrl(data)).rejects.toThrowError(NotFoundException);
+//     });
+//   });
+
+  // Add more test cases for other methods as needed
+
 });
